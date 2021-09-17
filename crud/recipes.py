b11aa5ff4279f429c142d281de3edc4b4be234a3
michalasia10 <misiek1027@o2.pl>
2021-09-17 19:03:49 +0200

    [feat] Create more basic endpoints for recipe

--- crud/recipes.py
+++ crud/recipes.py
@@ -1,12 +1,28 @@
 from sqlalchemy.orm import Session
 
 from models.models import Recipe
-from .repeated_crud.repetead import create, get_all
+from .repeated_crud.repetead import create, get_all, get_by_id_with_valid, delete, update
 
 
 def get_all_recipie(db: Session):
     return get_all(db, Recipe)
 
 
+def get_all_recipies_with_items(db: Session):
+    return get_all(db, Recipe)
+
+
+def get_recipe_by_id(db: Session, recipe_id: int):
+    return get_by_id_with_valid(db, Recipe, recipe_id).first()
+
+
 def create_recipe(db: Session, request):
     return create(db, Recipe, request)
+
+
+def delete_recipe(db: Session, recipe_id: int):
+    return delete(db, Recipe, recipe_id)
+
+
+def update_recipe(db: Session, recipe_id: int, request):
+    return update(db, Recipe, recipe_id, request)
