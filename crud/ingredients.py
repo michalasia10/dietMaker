8a6b327b8d51a286288410612f5de38f05f51d72
michalasia10 <misiek1027@o2.pl>
2021-09-17 19:02:20 +0200

    [feat] Added feat to update makro for recipe when user add ingredients

--- crud/ingredients.py
+++ crud/ingredients.py
@@ -15,12 +15,11 @@ def create_ingredient(db: Session, request):
     product_id = requestAsDict['product_id']
     product = get_by_id_with_valid(db, Product, product_id).first().__dict__
     productMakros = {makroKey: product[makroKey] for makroKey in product if makroKey in ('carbo', 'fat', 'protein')}
-    print(productMakros)
+
     recipe_id = requestAsDict['recipe_id']
     recipe = get_by_id_with_valid(db, Recipe, recipe_id).first().__dict__
     oldRecipeMakros = {makroKey: recipe[makroKey] for makroKey in recipe if makroKey in ('carbo', 'fat', 'protein')}
-    print(oldRecipeMakros)
     oldRecipeMakros.update(productMakros)
-    print(oldRecipeMakros)
+
     update(db, Recipe, recipe_id, oldRecipeMakros)
     return create_with_relation(db, Ingredient, request)
