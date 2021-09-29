2445d4d8526712c8d9bf2b13029ea2e5cb3ffab9
michalasia10 <misiek1027@o2.pl>
2021-09-29 17:28:02 +0200

    [heroku] heroku config v.05

--- db/database.py
+++ db/database.py
@@ -12,6 +12,6 @@ SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
