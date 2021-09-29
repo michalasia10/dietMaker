c42b7362f60c96f0f64bcab66318084ba1bda357
michalasia10 <misiek1027@o2.pl>
2021-09-29 21:12:35 +0200

    [fixed] fixed bug with units

--- db/database.py
+++ db/database.py
@@ -1,19 +1,18 @@
 import os
 
-
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+load_dotenv()
 
-# local
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./diet.db"
+DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+DATABASE_HOSTNAME = os.environ.get("DATABASE_HOSTNAME")
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
 
-# prod
-SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
-if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOSTNAME}/{DATABASE_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
