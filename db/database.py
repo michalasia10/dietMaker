c15d131e967e36d0b731726fff1e63c6dc538298
michalasia10 <misiek1027@o2.pl>
2021-09-29 19:57:31 +0200

    [feat] Plug in to postgress online for dev purpose

--- db/database.py
+++ db/database.py
@@ -1,9 +1,19 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./diet.db"
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args = {"check_same_thread":False})
-SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
+DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+DATABASE_HOSTNAME = os.environ.get("DATABASE_HOSTNAME")
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOSTNAME}/{DATABASE_NAME}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
