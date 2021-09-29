d96aa7d39f7cddc1cbe5cbe889c44891a7fb3c70
michalasia10 <misiek1027@o2.pl>
2021-09-29 17:11:27 +0200

    [heroku] heroku config

--- db/database.py
+++ db/database.py
@@ -1,9 +1,15 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./diet.db"
+# local
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./diet.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args = {"check_same_thread":False})
-SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
+# prod
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
