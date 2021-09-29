51ac5b7db7fc054b2d38fccfac7f68a0775555eb
michalasia10 <misiek1027@o2.pl>
2021-09-29 21:42:29 +0200

    [fixed] fixed bug with crud ingredients

--- crud/ingredients.py
+++ crud/ingredients.py
@@ -1,9 +1,9 @@
-import json
+from sqlalchemy.orm import Session
 
 from sqlalchemy.orm import Session
 
-from models.meal import Ingredient, Recipe, Product
-from .repeated_crud.repetead import get_by_id_with_valid, get_all, create_with_relation, update
+from models.meal import Ingredient
+from .repeated_crud.repetead import get_all, create
 
 
 def get_all_ingredietns(db: Session):
@@ -11,15 +11,4 @@ def get_all_ingredietns(db: Session):
 
 
 def create_ingredient(db: Session, request):
-    requestAsDict = json.loads(request.json())
-    product_id = requestAsDict['product_id']
-    product = get_by_id_with_valid(db, Product, product_id).first().__dict__
-    productMakros = {makroKey: product[makroKey] for makroKey in product if makroKey in ('carbo', 'fat', 'protein')}
-
-    recipe_id = requestAsDict['recipe_id']
-    recipe = get_by_id_with_valid(db, Recipe, recipe_id).first().__dict__
-    oldRecipeMakros = {makroKey: recipe[makroKey] for makroKey in recipe if makroKey in ('carbo', 'fat', 'protein')}
-    oldRecipeMakros.update(productMakros)
-
-    update(db, Recipe, recipe_id, oldRecipeMakros)
-    return create_with_relation(db, Ingredient, request)
+    return create(db, Ingredient, request)
