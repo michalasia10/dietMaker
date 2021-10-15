30cbaae7550532f2ae1b6161246dd6d377c49579
michalasia10 <misiek1027@o2.pl>
2021-10-15 22:16:40 +0200

    [config] Create config for db based on different .env ( prod / dev )

--- db/database.py
+++ db/database.py
@@ -1,16 +1,31 @@
 import os
-
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from distutils.util import strtobool
 
 # local
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./diet.db"
 
-SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
-if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+load_dotenv()
+
+DEBUG  = strtobool(os.environ.get('DEBUG'))
+
+SQLALCHEMY_DATABASE_URL = ''
+
+if DEBUG:
+    DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME")
+    DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
+    DATABASE_HOSTNAME = os.environ.get("DATABASE_HOSTNAME")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME")
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOSTNAME}/{DATABASE_NAME}"
+else:
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
