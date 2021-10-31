0bb9e9c485ca53f710b79389409f77e54feb6dfc
michalasia10 <misiek1027@o2.pl>
2021-10-31 23:50:56 +0100

    [server] ports

--- source/features/versions/router.py
+++ source/features/versions/router.py
@@ -13,6 +13,12 @@ from source.features.user.week.api.v0_3 import version as week_0_3
 ROUTERS_V_0_3 = [product_0_3, category_0_3, ingredient_0_3, recipe_0_3,
                  unit_0_3, settings_0_3, account_0_3, meal_0_3,week_0_3]
 
+
+basic = APIRouter(
+    prefix="",
+    tags=[""],
+)
+
 version_0_3 = APIRouter(
     prefix="/v0.3",
     tags=["v0.3"],
@@ -20,4 +26,5 @@ version_0_3 = APIRouter(
 
 
 for route in ROUTERS_V_0_3:
+    basic.include_router(route)
     version_0_3.include_router(route)
