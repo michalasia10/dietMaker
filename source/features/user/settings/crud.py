5691174dd6c67b68f5b9c38c21f1cae0660aa86f
michalasia10 <misiek1027@o2.pl>
2021-11-29 01:15:42 +0100

    [settings] modify get to get settings by user id not settings id

--- source/features/user/settings/crud.py
+++ source/features/user/settings/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 import json
+from source.features.user.account.models import User
 from source.features.user.settings.models import UserSetting
 from source.core.db_queries.crud  import get_by_id, simple_object_creator, delete
 
@@ -9,5 +10,6 @@ def create_settings(db:Session,request,user_id:int):
 def delete_settings(db:Session,settings_id:int):
     return delete(db,UserSetting,settings_id)
 
-def get_settings(db:Session,settings_id:int):
-    return get_by_id(db,UserSetting,settings_id)
+def get_settings(db:Session,user_id:int):
+    settings_id = get_by_id(db,User,user_id).first().settings[0].id
+    return get_by_id(db,UserSetting,settings_id).first()
