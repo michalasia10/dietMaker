517d3fa223815472eb9015832453f203db52e073
michalasia10 <misiek1027@o2.pl>
2021-11-29 01:14:15 +0100

    [meal] create endpoint to get user plan for timestamp

--- source/features/user/meal/schema.py
+++ source/features/user/meal/schema.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel
+from typing import List
+from source.features.user.week.schema import UserWeek
 
-class UserMeals(BaseModel):
+class UserMealBasic(BaseModel):
     name: str
+
+class UserMeals(UserMealBasic):
+    day_meals : List[UserWeek] = []
+
+    class Config:
+        orm_mode = True
