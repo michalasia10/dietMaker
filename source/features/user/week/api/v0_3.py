5f97072325751acdafae01f92d3195d1a8a3b1eb
michalasia10 <misiek1027@o2.pl>
2021-12-05 00:18:23 +0100

    [meal] create functionality to count macro for meal in day

--- source/features/user/week/api/v0_3.py
+++ source/features/user/week/api/v0_3.py
@@ -12,7 +12,7 @@ from source.features.user.meal.crud import get_users_meals_by_timestamp
 from source.core.app.database.get_db import get_db
 from source.features.user.week.schema import UserWeek
 
-version = APIRouter(
+userweek = APIRouter(
     prefix="/userweek",
     tags=["userweek"],
 )
@@ -21,8 +21,8 @@ plan = APIRouter(
     prefix="/plan",
     tags=["plan"],
 )
-@version.get('/',
-             response_model=List[UserWeek])
+@userweek.get('/',
+              response_model=List[UserWeek])
 def get_all(db: Session = Depends(get_db)):
     return get_all_week(db)
 
@@ -35,16 +35,16 @@ def add_recipe_to_plan(timestamp:int,meal_id:int,recipe_id:int,user_id:int, db: 
 def get_user_meals_by_timestamp_with_count(user_id : int, timestamp:int, count : int,db : Session=Depends(get_db)):
     return get_users_meals_by_timestamp(db,user_id,timestamp,count)
 
-@version.get('/{week_id}', response_model=UserWeek)
+@userweek.get('/{week_id}', response_model=UserWeek)
 def get_week_by_id(week_id: int, db: Session = Depends(get_db)):
     return get_week(db, week_id)
 
-@version.delete('/delete/{week_id}', response_model=UserWeek)
+@userweek.delete('/delete/{week_id}', response_model=UserWeek)
 def get_delete_by_id(week_id: int, db: Session = Depends(get_db)):
     return delete_week(db, week_id)
 
 
-@version.delete('/delete/{week_id}/{recipe_id}',status_code=status.HTTP_200_OK)
+@userweek.delete('/delete/{week_id}/{recipe_id}', status_code=status.HTTP_200_OK)
 def delete_recipe_week(week_id:int,recipe_id:int,db:Session=Depends(get_db)):
     return delete_recipe_from_week(db,week_id,recipe_id)
 
--- source/features/user/week/schema.py
+++ source/features/user/week/schema.py
@@ -1,12 +1,40 @@
 from pydantic import BaseModel,validator
 from typing import List,Optional
 from datetime import datetime
+
+
 from source.features.recipe.schema import RecipeInMeal
+
+
+
+class Macro(BaseModel):
+    protein: Optional[float] = 0
+    fat: Optional[float] = 0
+    carbo: Optional[float] = 0
+    kcal: Optional[float] = 0
+
+    @validator('protein', pre=True)
+    def protein_validate(cls, dt):
+        return round(dt,3)
+
+    @validator('fat', pre=True)
+    def fat_validate(cls, dt):
+        return round(dt,3)
+
+    @validator('carbo', pre=True)
+    def carbo_validate(cls, dt):
+        return round(dt,3)
+
+    @validator('kcal', pre=True)
+    def kcal_validate(cls, dt):
+        return round(dt,3)
+
 
 class UserWeek(BaseModel):
     id: int
     date:Optional[int] = 0
     recipes: List[RecipeInMeal] = []
+    macro : Macro = {}
 
     @validator('date',pre=True)
     def date_validate(cls,dt):
